3184504524d0044537978645f30c14af9856f3c6
Bryce Aebi <bryce.a.aebi@gmail.com>
2016-11-12 21:28:33 -0800

    Untested api wrappers

--- Assist/AssistClient.swift
+++ Assist/AssistClient.swift
@@ -7,98 +7,254 @@
 //
 
 import UIKit
+import Alamofire
 
 // TODO: move these somewhere intelligent
-let devURLString = "http://127.0.0.1:5000/"
+let devURLString = "http://127.0.0.1:8000/api"
 let prodURLString = ""
 
 
-// TODO add error handling
 class AssistClient: NSObject {
-    
+
     static let sharedInstance = AssistClient(baseURLString: devURLString)
-    
+
     var baseURLString: String!
-    var urlSession: URLSession!
-    
+
     init(baseURLString: String) {
         super.init()
         self.baseURLString = baseURLString
-        self.urlSession = URLSession(
-            configuration: URLSessionConfiguration.default,
-            delegate: nil,
-            delegateQueue: OperationQueue.main
-        )
     }
-    
+
+
+    /********** CLIENT API **********/
+
     func createClient(
-        clientDict: [NSDictionary],
+        clientDict: Dictionary<String, String>,
         success: @escaping (Client) -> (),
         failure: @escaping (Error) -> ()
     ) {
-        let url = URL(string: "\(baseURLString)/clients/create")
-        let request = URLRequest(url: url!)
-        let task: URLSessionDataTask = self.urlSession.dataTask(
-            with: request,
-            completionHandler: { (dataOrNil, response, error) in
-                if let data = dataOrNil {
-                    if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                        print("response: \(responseDictionary)")
-                        success(Client(clientDict: responseDictionary))
+        Alamofire.request(
+            "\(baseURLString)/clients/create",
+            method: .post,
+            parameters: clientDict
+        ).validate().responseJSON(
+            completionHandler: { (response: DataResponse<Any>) in
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                    if let JSON = response.result.value {
+                        print("JSON: \(JSON)")
                     }
+                case .failure(let error):
+                    failure(error)
                 }
             }
         )
-        task.resume()
     }
-    
-    func updateClient(
+
+    func getClient(
         clientID: String,
         success: @escaping (Client) -> (),
         failure: @escaping (Error) -> ()
     ) {
-        // let url = URL(string: "\(baseURLString)/clients/\(clientID)/update")
-        // let request = URLRequest(url: url!)
+        Alamofire.request(
+            "\(baseURLString)/clients/\(clientID)",
+            method: .get
+        ).validate().responseJSON { (response: DataResponse<Any>) in
+            switch response.result {
+            case .success:
+                print("Validation Successful")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
     }
-    
+
+    func updateClient(
+        clientID: String,
+        clientDict: Dictionary<String, String>,
+        success: @escaping (Client) -> (),
+        failure: @escaping (Error) -> ()
+    ) {
+        Alamofire.request(
+            "\(baseURLString)/clients/\(clientID)",
+            method: .patch,
+            parameters: clientDict
+        ).validate().responseJSON { (response: DataResponse<Any>) in
+            switch response.result {
+            case .success:
+                print("Validation Successful")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+
     func deleteClient(
         clientID: String,
-        success: @escaping () ->(),
-        failure: @escaping (Error) -> ()
-    ) {
-        // let url = URL(string: "\(baseURLString)/clients/\(clientID)/delete")
-        // let request = URLRequest(url: url!)
-    }
-    
-    func getTasks(
-        taskIDs: [String],
-        success: @escaping ([Task]) -> (),
-        failure: @escaping (Error) -> ()
-    ) {
-        
-    }
-    
-    func createTasks(
-        taskDicts: [NSDictionary],
-        success: @escaping ([Task]) -> (),
-        failure: @escaping (Error) -> ()
-    ) {
-        
-    }
-    
-    func updateTasks(
-        taskIDs: [String],
-        success: @escaping ([Task]) -> (),
-        failure: @escaping (Error) -> ()
-    ) {
-        
-    }
-    
-    func deleteTasks(
-        taskIDs: [String],
         success: @escaping () -> (),
         failure: @escaping (Error) -> ()
     ) {
-        
+        Alamofire.request(
+            "\(baseURLString)/clients/\(clientID)",
+            method: .delete
+        ).validate().responseJSON { (response: DataResponse<Any>) in
+            switch response.result {
+            case .success:
+                print("Validation Successful")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+
+
+    /********** ASSISTANT API **********/
+
+    func getAssistant(
+        assistantID: String,
+        success: @escaping (Client) -> (),
+        failure: @escaping (Error) -> ()
+    ) {
+        Alamofire.request(
+            "\(baseURLString)/assistants/\(assistantID)",
+            method: .get
+        ).validate().responseJSON { (response: DataResponse<Any>) in
+            switch response.result {
+            case .success:
+                print("Validation Successful")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+
+
+    /********** TASK API **********/
+
+    func getTasksForClient(
+        clientID: String,
+        success: @escaping ([Task]) -> (),
+        failure: @escaping (Error) -> ()
+    ) {
+        Alamofire.request(
+            "\(baseURLString)/clients/\(clientID)/tasks",
+            method: .get
+        ).validate().responseJSON { (response: DataResponse<Any>) in
+            switch response.result {
+            case .success:
+                print("Validation Successful")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+
+    func getTask(
+        taskID: String,
+        clientID: String,
+        success: @escaping (Task) -> (),
+        failure: @escaping (Error) -> ()
+    ) {
+        Alamofire.request(
+            "\(baseURLString)/clients/\(clientID)tasks/\(taskID)",
+            method: .get
+        ).validate().responseJSON { (response: DataResponse<Any>) in
+            switch response.result {
+            case .success:
+                print("Validation Successful")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+
+    func createTask(
+        clientID: String,
+        taskDict: Dictionary<String, String>,
+        success: @escaping (Task) -> (),
+        failure: @escaping (Error) -> ()
+    ) {
+        Alamofire.request(
+            "\(baseURLString)/clients/\(clientID)/tasks",
+            method: .post,
+            parameters: taskDict
+        ).validate().responseJSON(
+            completionHandler: { (response: DataResponse<Any>) in
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                    if let JSON = response.result.value {
+                        print("JSON: \(JSON)")
+                    }
+                case .failure(let error):
+                    failure(error)
+                }
+            }
+        )
+    }
+
+    func updateTask(
+        taskID: String,
+        clientID: String,
+        taskDict: Dictionary<String,String>,
+        success: @escaping (Task) -> (),
+        failure: @escaping (Error) -> ()
+    ) {
+        Alamofire.request(
+            "\(baseURLString)/clients/\(clientID)/tasks/\(taskID)",
+            method: .patch,
+            parameters: taskDict
+        ).validate().responseJSON { (response: DataResponse<Any>) in
+            switch response.result {
+            case .success:
+                print("Validation Successful")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+
+    func deleteTask(
+        clientID: String,
+        taskID: String,
+        success: @escaping () -> (),
+        failure: @escaping (Error) -> ()
+    ) {
+        Alamofire.request(
+            "\(baseURLString)/clients/\(clientID)/tasks\(taskID)",
+            method: .delete
+        ).validate().responseJSON { (response: DataResponse<Any>) in
+            switch response.result {
+            case .success:
+                print("Validation Successful")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
     }
 }
